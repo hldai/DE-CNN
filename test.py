fb78cc12127b3be1f229efeacdfbc608c2b45eb3
hldai <hldai@outlook.com>
2019-05-27 19:23:49 +0800

    update

--- test.py
+++ test.py
@@ -385,9 +385,15 @@ if __name__ == "__main__":
 
     # evaluate(args.runs, args.data_dir, args.model_dir, args.domain, command, template)
 
-    data_file = 'data/prep_data/laptops14-dhl.npz'
-    # data_file = 'data/prep_data/laptop.npz'
-    text_file = 'data/prep_data/laptops14-dhl-test-raw.json'
-    pred_file = 'data/official_data/pred-dhl.xml'
-    gold_file = 'data/official_data/Laptops_Test_Gold.xml'
+    if args.domain=='laptop':
+        # data_file = 'data/prep_data/laptop.npz'
+        data_file = 'data/prep_data/laptops14-dhl.npz'
+        text_file = 'data/prep_data/laptops14-dhl-test-raw.json'
+        pred_file = 'data/official_data/pred-dhl-lap.xml'
+        gold_file = 'data/official_data/Laptops_Test_Gold.xml'
+    else:
+        data_file = 'data/prep_data/restaurants14-dhl.npz'
+        text_file = 'data/prep_data/restaurants14-dhl-test-raw.json'
+        pred_file = 'data/official_data/pred-dhl-rest.xml'
+        gold_file = 'data/official_data/Restaurants_Test_Gold.xml'
     evaluate_dhl(args.runs, data_file, text_file, args.model_dir, args.domain, template, gold_file, pred_file)
